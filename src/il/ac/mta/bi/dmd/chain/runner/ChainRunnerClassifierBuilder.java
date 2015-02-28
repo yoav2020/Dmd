08d4f7b756f39ffddbfe7653f0f8699d52ceec38
yoav <yoav@checkpoint.com>
2015-02-28 23:55:14 +0200

    Description:

---
+++ src/il/ac/mta/bi/dmd/chain/runner/ChainRunnerClassifierBuilder.java
@@ -0,0 +1,99 @@
+package il.ac.mta.bi.dmd.chain.runner;
+
+import il.ac.mta.bi.dmd.common.ClassifierWrapper;
+import il.ac.mta.bi.dmd.common.DomainToAnalyze.Classification;
+import il.ac.mta.bi.dmd.common.ProcessChain;
+import il.ac.mta.bi.dmd.common.ProcessingChain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import weka.classifiers.Evaluation;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+
+/**
+ * This chain builds the classifier for a given arff. It also trains the classifier and
+ * tests it, periodically. It fills the property ""myClassifier" - the classifier instance
+ * to be used for classifying the domain
+ */
+
+public class ChainRunnerClassifierBuilder extends ProcessChain {
+	static Logger 	logger = Logger.getLogger(ChainRunnerClassifierBuilder.class);
+	
+	private static Map<Integer,ClassifierWrapper> classifierMap = new HashMap<>();
+	private static final Integer CLASS_TEST_RATIO = 4;
+	
+	public ChainRunnerClassifierBuilder() {
+		setChainName("Classifier Builder Runner");
+	}
+
+	@Override
+	public void run() {
+		try {
+			Integer fvWekaAttributesHash = 
+					(Integer) domainToAnalyze.getPropertiesMap().get("fvWekaAttributesHash");
+
+			Instance instanceData = 
+					(Instance) domainToAnalyze.getPropertiesMap().get("instanceData");
+			
+			ClassifierWrapper classifierWrapper = classifierMap.get(fvWekaAttributesHash);
+			if (classifierWrapper == null) {
+				classifierWrapper = generateClassifier();
+				classifierMap.put(fvWekaAttributesHash, classifierWrapper);
+			}		
+			
+			/* if we are certain of the domain classification - use it for
+			 * training or evaluation testing
+			 */
+			
+			if (domainToAnalyze.getClassification() != Classification.UNKNOWN) {
+				maintainModel(instanceData, classifierWrapper);
+			} 
+			
+			/* insert the classifier to the map */
+			
+			domainToAnalyze.getPropertiesMap().put("myClassifier", classifierWrapper);
+		} catch (Exception e) {
+			logger.error("caught exception ", e);
+			setStatus(ProcessingChain.chainStatus.ERROR);
+		}
+		
+		flush();
+	}
+
+	private void maintainModel(Instance instanceData,
+			ClassifierWrapper classifierWrapper) throws Exception {
+		if (classifierWrapper.getTotalClassifications() % CLASS_TEST_RATIO == 
+				CLASS_TEST_RATIO-1) {
+			logger.info("testing classifier");
+			classifierWrapper.evaluateModel(instanceData);
+			logger.info(classifierWrapper.getStats());
+		} else {
+			logger.info("training classifier");
+			classifierWrapper.updateClassifier(instanceData);
+		}
+	}
+
+	private ClassifierWrapper generateClassifier() throws Exception {
+		FastVector fvWekaAttribute = 
+				(FastVector) domainToAnalyze.getPropertiesMap().get("fvWekaAttributes");
+		Instances dataSet = 
+				new Instances("Domain class relation", fvWekaAttribute, 0);
+		ClassifierWrapper classifierWrapper = 
+				new ClassifierWrapper();
+
+		dataSet.setClassIndex(dataSet.numAttributes()-1);
+		classifierWrapper.buildClassifier(dataSet);
+		classifierWrapper.setDataSet(dataSet);
+		classifierWrapper.setEval(new Evaluation(dataSet));
+		
+		logger.info("classifier created successfully!");
+		
+		return classifierWrapper;
+	}
+
+}
---
+++ src/il/ac/mta/bi/dmd/chain/runner/ChainRunnerClassify.java
@@ -0,0 +1,51 @@
+package il.ac.mta.bi.dmd.chain.runner;
+
+import il.ac.mta.bi.dmd.common.ClassifierWrapper;
+import il.ac.mta.bi.dmd.common.DomainToAnalyze.Classification;
+import il.ac.mta.bi.dmd.common.ProcessChain;
+import il.ac.mta.bi.dmd.common.ProcessingChain;
+
+import org.apache.log4j.Logger;
+
+import weka.core.Instance;
+
+/**
+ * The classification chain, based on the classifier created in the previous chains
+ */
+
+public class ChainRunnerClassify extends ProcessChain {
+	static Logger 	logger = Logger.getLogger(ChainRunnerClassify.class);
+
+	public ChainRunnerClassify() {
+		setChainName("Classifier Runner");
+	}
+
+	@Override
+	public void run() {
+		try {
+			if (domainToAnalyze.getClassification() == Classification.UNKNOWN) {
+				logger.info("classifying domain " + domainToAnalyze.getDomainName());
+				ClassifierWrapper classifierWrapper = 
+						(ClassifierWrapper) domainToAnalyze.getPropertiesMap().get("myClassifier");
+				Instance instanceData = 
+						(Instance) domainToAnalyze.getPropertiesMap().get("instanceData");
+				double[] result = classifierWrapper.classifyInstance(instanceData);
+				
+				logger.info("malicious chance=" + result[0]);
+				logger.info("benign chance=" + result[1]);
+				
+				System.out.println(domainToAnalyze.getDomainName() + " class: ");
+				System.out.println("malicious chance=" + result[0]);
+				System.out.println("benign chance=" + result[1]);
+				System.out.println();
+			} else {
+				logger.info("domain type is known, nothing to do");
+			}
+		} catch (Exception e) {
+			logger.error("caught exception ", e);
+			setStatus(ProcessingChain.chainStatus.ERROR);
+		}
+		
+		flush();
+	}
+}
