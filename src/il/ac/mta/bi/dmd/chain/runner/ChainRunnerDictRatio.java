ae1ab6766af83cc629466abd43b3dcdba03d1556
IsraelServ <israelserv@gmail.com>
2015-03-12 19:31:00 +0200

    Disable threads on Chain - Dictionary Ratio of domain name

--- src/il/ac/mta/bi/dmd/chain/runner/ChainRunnerDictRatio.java
+++ src/il/ac/mta/bi/dmd/chain/runner/ChainRunnerDictRatio.java
@@ -31,12 +31,8 @@ import il.ac.mta.bi.dmd.infra.Factory;
 public class ChainRunnerDictRatio extends ProcessChain {
 	static Logger logger = Logger.getLogger(ChainRunnerDictRatio.class);
 
-	private static final LinkedBlockingQueue<ChainRunnerDictRatio> inQueue =
-			new LinkedBlockingQueue<>();
-	private static Boolean 			isConsumerRunning = false;
-	private static final Integer	MAX_THREAD_COUNT  = 64;
+	private static Boolean 			isFirstRun = true;
 	private static final Trie 		trie			  = new Trie();
-	private static Semaphore 		chainRunnerDictRatioSemaphore;
 	private static final String DICTIONARY_FILE = "data\\US.dic";
 	
 	public ChainRunnerDictRatio() {
@@ -44,13 +40,13 @@ public class ChainRunnerDictRatio extends ProcessChain {
 		
 		chainFeaturesList.add(Factory.getFactory().getIntegerFeature("dictRatio"));
 		
-		if (isConsumerRunning == false) {
+		if (isFirstRun == true) {
 			firstChainInit();
 		}
 	}
 
 	private void firstChainInit() {
-		if (isConsumerRunning == false) {
+		if (isFirstRun == true) {
 			
 			try {
 				readFileIntoTrie(new File(DICTIONARY_FILE), trie);
@@ -59,9 +55,7 @@ public class ChainRunnerDictRatio extends ProcessChain {
 				e.printStackTrace();
 			}
 			
-			Factory.getFactory().execForRunnableTask(new ChainRunnerDictRatioConsumer());
-			chainRunnerDictRatioSemaphore = new Semaphore(MAX_THREAD_COUNT, true);
-			isConsumerRunning = true;
+			isFirstRun = false;
 		}
 	}
 	
@@ -83,63 +77,9 @@ public class ChainRunnerDictRatio extends ProcessChain {
 	
 	@Override
 	public void run() {
+		float percent = 0;
+		chainStatus chainRunnerStatus = ProcessingChain.chainStatus.OK;
 		try {
-			inQueue.add(this);
-		} catch (Exception e) {
-			logger.error("caught exception ", e);
-			setStatus(ProcessingChain.chainStatus.ERROR);
-			flush();
-		}
-	}
-	
-
-	/* internal private class for consumer thread; calls the working
-	 * to process input */
-	private class ChainRunnerDictRatioConsumer implements Runnable {
-		@Override
-		public void run() {
-			while (true) {
-				ChainRunnerDictRatio chainRunner = null;
-				chainStatus chainRunnerStatus = ProcessingChain.chainStatus.OK;
-				
-				try {				
-					chainRunner = inQueue.take();
-					
-					/* limit number of concurrent Dictionary Ratio
-					 * threads, so not to exhaust the thread pool
-					 */
-					chainRunnerDictRatioSemaphore.acquire();
-					
-					Factory.getFactory().
-					getExecutorForCallableTask(new ChainRunnerDictRatioWorker(chainRunner));
-				} catch (Exception e) {
-					logger.error("caught exception ", e);
-					chainRunnerStatus = ProcessingChain.chainStatus.ERROR;
-					chainRunner.setStatus(chainRunnerStatus);
-					chainRunner.flush();
-				}
-			}	
-		}
-	}
-	
-
-	/* internal private class for worker thread; returns a Future reference
-	 * to the object called so it can be used to check return value */
-	private class ChainRunnerDictRatioWorker implements Callable<Object>{
-		ChainRunnerDictRatio chainRunner = null;
-		
-		public ChainRunnerDictRatioWorker (ChainRunnerDictRatio chainRunner) {
-			this.chainRunner = chainRunner;
-		}
-			
-		@Override
-		public Object call() {
-			float percent = 0;
-			chainStatus chainRunnerStatus = ProcessingChain.chainStatus.OK;
-			
-			try {				
-				DomainToAnalyze domainToAnalyze = chainRunner.domainToAnalyze;
-				
 				logger.info("creating Dictionary Ratio request for " + domainToAnalyze.getDomainName());
 				
 				Collection<Emit> emits = trie.parseText(domainToAnalyze.getDomainName().split(Pattern.quote("."))[0]);
@@ -156,18 +96,13 @@ public class ChainRunnerDictRatio extends ProcessChain {
 				feature.setValue(Math.round(percent));
 				featuresMap.put(feature.getName(), feature);
 				propertiesMap.put(feature.getName(), percent);
-			} catch (Exception e) {
-				logger.error("caught exception ", e);
-				chainRunnerStatus = ProcessingChain.chainStatus.ERROR;
-			}
-			finally {
-				chainRunner.setStatus(chainRunnerStatus);
-				chainRunner.flush();
-				chainRunnerDictRatioSemaphore.release();
-			}
-			
-			return this;
+		} catch (Exception e) {
+			logger.error("caught exception ", e);
+			chainRunnerStatus = ProcessingChain.chainStatus.ERROR;
+		}
+		finally {
+			setStatus(chainRunnerStatus);
+			flush();
 		}
 	}
-
 }
